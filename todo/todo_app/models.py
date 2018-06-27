536bc5c56781b48ac78d66cd07b89cd728f9f599
jeevan437 <jeevanpjr@gmail.com>
2018-06-27 16:18:41 +0530

    added models

--- todo/todo_app/models.py
+++ todo/todo_app/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+STATUS_CHOICES = (
+    ('PARKED', 'PARKED'),
+    ('STARTED', 'Started'),
+    ('FINISHED','Finished')
+)
+class todo_model(models.Model):
+
+    task_name = models.CharField(max_length=100, unique=True)
+    task_description =models.TextField()
+    task_status = models.CharField(max_length=100,choices=STATUS_CHOICES)
+
+    def __str__(self):
+        return self.task_name
