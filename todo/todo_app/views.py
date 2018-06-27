e0948ce79fc4989f02642946fc54d76c2983c992
jeevan437 <jeevanpjr@gmail.com>
2018-06-27 16:41:06 +0530

    added app level urls and views

--- todo/todo_app/views.py
+++ todo/todo_app/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,render_to_response
+from .models import todo_model
 # Create your views here.
+
+
+def index(request):  # Define our function, accept a request
+
+    items = todo_model.objects.all()  # ORM queries the database for all of the to-do entries.
+
+    return render_to_response('index.html', {'items': items})
+    #return render_to_response(items)
