e4342d1611892c488975ce05b6a23ee0fc60274d
jeevan437 <jeevanpjr@gmail.com>
2018-06-28 14:05:19 +0530

    created todo aplication

--- todo/todo_app/models.py
+++ todo/todo_app/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 STATUS_CHOICES = (
-    ('PARKED', 'PARKED'),
+    ('PARKED', 'Parked'),
     ('STARTED', 'Started'),
     ('FINISHED','Finished')
 )
