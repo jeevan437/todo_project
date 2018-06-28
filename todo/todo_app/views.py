eaba1c1f21f53b4ae37ac86ce6a0733f473cf2b2
jeevan437 <jeevanpjr@gmail.com>
2018-06-28 10:29:13 +0530

    modified in views.py

--- todo/todo_app/views.py
+++ todo/todo_app/views.py
@@ -15,19 +15,3 @@ def index_view(request):  # Define our function, accept a request
     #return render_to_response(items)
 
 
-@api_view(['GET', 'POST'])
-def todo_list(request):
-    """
-    List all code snippets, or create a new snippet.
-    """
-    if request.method == 'GET':
-        todo_data = todo_model.objects.all()
-        serializer = todo_serializer(todo_data, many=True)
-        return Response(serializer.data)
-
-    elif request.method == 'POST':
-        serializer = todo_serializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
